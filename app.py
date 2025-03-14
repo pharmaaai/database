6a93cd61ed669860ccc1547bb24ddd824eb83adf
PHARMA AI <pharmaai.online@gmail.com>
2025-03-14 11:21:14 +0530

    app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+from streamlit.components.v1 import html
 from pinecone import Pinecone, ServerlessSpec
 from sentence_transformers import SentenceTransformer
 from langgraph.graph import StateGraph, END
@@ -258,16 +259,43 @@ def main_application():
 
 main_application()
 
-# Add Tableau visualization at bottom
-st.markdown("""
-<div class='tableauPlaceholder' id='viz1738751341715' style='position: relative'><noscript><a href='#'><img alt='Life Science ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;nf&#47;nfljnvjlnjvlnjlv&#47;LifeScience&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='nfljnvjlnjvlnjlv&#47;LifeScience' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;nf&#47;nfljnvjlnjvlnjlv&#47;LifeScience&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='language' value='en-US' /><param name='filter' value='publish=yes' /></object></div>                
-<script type='text/javascript'>                    
-    var divElement = document.getElementById('viz1738751341715');                    
-    var vizElement = divElement.getElementsByTagName('object')[0];                    
-    vizElement.style.width='100%';
-    vizElement.style.height=(divElement.offsetWidth*0.75)+'px';                    
-    var scriptElement = document.createElement('script');                    
-    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    
-    vizElement.parentNode.insertBefore(scriptElement, vizElement);                
+# Add Tableau visualization at the bottom
+st.markdown("<hr>", unsafe_allow_html=True)
+st.subheader("Industry Insights Dashboard")
+
+# Use components.html for proper embedding
+from streamlit.components.v1 import html
+
+tableau_code = """
+<div class='tableauPlaceholder' id='viz1738751341715' style='position: relative'>
+<noscript><a href='#'><img alt='Life Science ' src='https://public.tableau.com/static/images/nf/nfljnvjlnjvlnjlv/LifeScience/1_rss.png' style='border: none' /></a></noscript>
+<object class='tableauViz' style='display:none;'>
+<param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' />
+<param name='embed_code_version' value='3' />
+<param name='site_root' value='' />
+<param name='name' value='nfljnvjlnjvlnjlv/LifeScience' />
+<param name='tabs' value='no' />
+<param name='toolbar' value='yes' />
+<param name='static_image' value='https://public.tableau.com/static/images/nf/nfljnvjlnjvlnjlv/LifeScience/1.png' />
+<param name='animate_transition' value='yes' />
+<param name='display_static_image' value='yes' />
+<param name='display_spinner' value='yes' />
+<param name='display_overlay' value='yes' />
+<param name='display_count' value='yes' />
+<param name='language' value='en-US' />
+<param name='filter' value='publish=yes' />
+</object>
+</div>
+<script type='text/javascript'>
+var divElement = document.getElementById('viz1738751341715');
+var vizElement = divElement.getElementsByTagName('object')[0];
+vizElement.style.width = '100%';
+vizElement.style.height = (divElement.offsetWidth * 0.75) + 'px';
+var scriptElement = document.createElement('script');
+scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
+vizElement.parentNode.insertBefore(scriptElement, vizElement);
 </script>
-""", unsafe_allow_html=True)
+"""
+
+html(tableau_code, width=1200, height=900)
+
